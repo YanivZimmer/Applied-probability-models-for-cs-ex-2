88b219b67a0ec406a402d5eda7bec37bd4244290
YanivZimmer <zimmer.yaniv@gmail.com>
2021-11-24 20:21:30 +0200

    count vocab and event occurrences from events, solution for output 10,11

--- code/main.py
+++ code/main.py
@@ -22,5 +22,10 @@ def split_train_validation(events,split_rate):
 train_dev,val_dev=split_train_validation(events_development_file,0.9)
 Output8=len(val_dev)
 Output9=len(train_dev)
-print("{0}+{1}={2}".format(Output8,Output9,Output9+Output8))
-print(Output7)    
+def count_unique_events(events):
+    vocab=set(events)
+    return len(vocab)
+Output10=count_unique_events(train_dev)
+def count_event_in_events(event,events):
+    return events.count(event)
+Output11=count_event_in_events(Output3,train_dev)
